2ac641319c971d2185acf51d2cce6e62bccb86ac
Sebastian <sisandoval@uc.cl>
2023-12-28 22:44:31 -0300

    this is a test commit

--- init.el
+++ init.el
@@ -25,9 +25,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(deeper-blue)))
+ '(package-selected-packages '(magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+
+;; this is a comment
