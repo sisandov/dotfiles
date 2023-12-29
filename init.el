1f35d3f404d299c39fbd2c3c3a48762b9f7ab959
Sebastian <sisandoval@uc.cl>
2023-12-28 22:50:22 -0300

    delete comments

--- init.el
+++ init.el
@@ -20,18 +20,8 @@
 ;;theme
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(deeper-blue)))
  '(package-selected-packages '(magit)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (require 'package)
 (add-to-list 'package-archives
