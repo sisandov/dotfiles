192fad64fcf28e77dbf9744128b2922f6866f28b
Sebastian <sisandoval@uc.cl>
2024-01-06 23:54:09 -0300

    orgmode, new init

---
+++ init_3.el
@@ -0,0 +1,14 @@
+(org-babel-load-file "~/dotfiles/init.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(aggressive-indent aggressive-indent-mode treemacs tree-sitter-langs smartparens pkg-info nano-modeline moe-theme magit file-info company auto-complete)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
